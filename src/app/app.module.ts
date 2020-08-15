f0a0a41436048fa565c6bb748f4ca0e16276bcea
abhishekfx505dy <abhishek.fx505dy@gmail.com>
2020-08-15 18:28:42 +0530

    first few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    FooterComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = 'Abhishek'
+  password = ''
+
+  invalidLogin = false
+
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+     console.log(this.username);
+     
+    if(this.username==="Abhishek" ){
+    this.router.navigate(['welcome', this.username]);
+      this.invalidLogin=false;
+      
+    }
+    else{
+      this.invalidLogin=true;
+    }
   }
 
 }
